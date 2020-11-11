7d292d0b11b592c36120d9aab0bfcdbc97f59538
yanottamao <oktatama4@gmail.com>
2020-11-11 13:36:08 +0700

    Initial streamlining

--- ardmove_foredit.py
+++ ardmove_foredit.py
@@ -4,30 +4,56 @@ from std_msgs.msg import String
 from std_msgs.msg import Empty
 from geometry_msgs.msg import Twist
 
+
+def kecepatandefault():
+    global kecepatan = input('Masukkan kecepatan antara 0.1 - 1: ')
+    return kecepatan
+
+
+def modeterbang():
+    print('Pilihan mode terbang:')
+    print('1. Take off / terbang')
+    print('2. Land / mendarat')
+    print('3. Mode masukan arah')
+    global mode
+    modeinput = int(input('Masukkan mode terbang: '))
+    if modeinput == 1:
+        mode = 'ardrone/takeoff'
+    elif modeinput == 2:
+        mode = 'ardrone/land'
+    elif modeinput == 3:
+        mode = '/cmd_vel'
+    else:
+        print('Pilih antara pilian diatas!')
+        modeterbang()
+
+
 def takeoff():
-    pub = rospy.Publisher("ardrone/takeoff", Empty, queue_size=10 )
+    pub = rospy.Publisher('ardrone/takeoff', Empty, queue_size=10)
     rospy.init_node('takeoff', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+    rate = rospy.Rate(10)  # 10hz
     while not rospy.is_shutdown():
         pub.publish(Empty())
         rate.sleep()
+
 
 def land():
-    pub = rospy.Publisher("ardrone/land", Empty, queue_size=10 )
+    pub = rospy.Publisher('ardrone/land', Empty, queue_size=10)
     rospy.init_node('land', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+    rate = rospy.Rate(10)  # 10hz
     while not rospy.is_shutdown():
         pub.publish(Empty())
         rate.sleep()
 
+
 def forward():
-    pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10 )
+    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
     rospy.init_node('forward', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+    rate = rospy.Rate(10)  # 10hz
     vel_msg = Twist()
     # parameter
-    kecepatan = input("Masukkan kecepatan: ")
-    jarak = input("Masukkan jarak")
+    kecepatan = input('Masukkan kecepatan: ')
+    jarak = input('Masukkan jarak')
 
     vel_msg.linear.x = abs(kecepatan)
     vel_msg.linear.y = 0
@@ -45,23 +71,24 @@ def forward():
         while(jarak_sekarang < jarak):
             # publish kecepatan
             pub.publish(vel_msg)
-            # ambil waktu sekarang 
+            # ambil waktu sekarang
             t1 = rospy.Time.now().to_sec()
             # kalkulasi jarak
             jarak_sekarang = kecepatan*(t1-t0)
-        # kecepatan 0 
+        # kecepatan 0
         vel_msg.linear.x = 0
         # paksa henti
         pub.publish(vel_msg)
 
+
 def backward():
-    pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10 )
+    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
     rospy.init_node('backward', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+    rate = rospy.Rate(10)  # 10hz
     vel_msg = Twist()
     # parameter
-    kecepatan = input("Masukkan kecepatan: ")
-    jarak = input("Masukkan jarak")
+    kecepatan = input('Masukkan kecepatan: ')
+    jarak = input('Masukkan jarak')
 
     vel_msg.linear.x = -abs(kecepatan)
     vel_msg.linear.y = 0
@@ -79,23 +106,24 @@ def backward():
         while(jarak_sekarang < jarak):
             # publish kecepatan
             pub.publish(vel_msg)
-            # ambil waktu sekarang 
+            # ambil waktu sekarang
             t1 = rospy.Time.now().to_sec()
             # kalkulasi jarak
             jarak_sekarang = kecepatan*(t1-t0)
-        # kecepatan 0 
+        # kecepatan 0
         vel_msg.linear.x = 0
         # paksa henti
         pub.publish(vel_msg)
 
+
 def toleft():
-    pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10 )
+    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
     rospy.init_node('toleft', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+    rate = rospy.Rate(10)  # 10hz
     vel_msg = Twist()
     # parameter
-    kecepatan = input("Masukkan kecepatan: ")
-    jarak = input("Masukkan jarak")
+    kecepatan = input('Masukkan kecepatan: ')
+    jarak = input('Masukkan jarak')
 
     vel_msg.linear.x = 0
     vel_msg.linear.y = abs(kecepatan)
@@ -113,23 +141,24 @@ def toleft():
         while(jarak_sekarang < jarak):
             # publish kecepatan
             pub.publish(vel_msg)
-            # ambil waktu sekarang 
+            # ambil waktu sekarang
             t1 = rospy.Time.now().to_sec()
             # kalkulasi jarak
             jarak_sekarang = kecepatan*(t1-t0)
-        # kecepatan 0 
+        # kecepatan 0
         vel_msg.linear.x = 0
         # paksa henti
         pub.publish(vel_msg)
 
+
 def toright():
-    pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10 )
+    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
     rospy.init_node('toright', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+    rate = rospy.Rate(10)  # 10hz
     vel_msg = Twist()
     # parameter
-    kecepatan = input("Masukkan kecepatan: ")
-    jarak = input("Masukkan jarak")
+    kecepatan = input('Masukkan kecepatan: ')
+    jarak = input('Masukkan jarak')
 
     vel_msg.linear.x = 0
     vel_msg.linear.y = -abs(kecepatan)
@@ -147,23 +176,24 @@ def toright():
         while(jarak_sekarang < jarak):
             # publish kecepatan
             pub.publish(vel_msg)
-            # ambil waktu sekarang 
+            # ambil waktu sekarang
             t1 = rospy.Time.now().to_sec()
             # kalkulasi jarak
             jarak_sekarang = kecepatan*(t1-t0)
-        # kecepatan 0 
+        # kecepatan 0
         vel_msg.linear.x = 0
         # paksa henti
         pub.publish(vel_msg)
 
+
 def upward():
-    pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10 )
+    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
     rospy.init_node('upward', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+    rate = rospy.Rate(10)  # 10hz
     vel_msg = Twist()
     # parameter
-    kecepatan = input("Masukkan kecepatan: ")
-    jarak = input("Masukkan jarak")
+    kecepatan = input('Masukkan kecepatan: ')
+    jarak = input('Masukkan jarak')
 
     vel_msg.linear.x = 0
     vel_msg.linear.y = 0
@@ -181,23 +211,24 @@ def upward():
         while(jarak_sekarang < jarak):
             # publish kecepatan
             pub.publish(vel_msg)
-            # ambil waktu sekarang 
+            # ambil waktu sekarang
             t1 = rospy.Time.now().to_sec()
             # kalkulasi jarak
             jarak_sekarang = kecepatan*(t1-t0)
-        # kecepatan 0 
+        # kecepatan 0
         vel_msg.linear.x = 0
         # paksa henti
         pub.publish(vel_msg)
 
+
 def downward():
-    pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10 )
+    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
     rospy.init_node('downward', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+    rate = rospy.Rate(10)  # 10hz
     vel_msg = Twist()
     # parameter
-    kecepatan = input("Masukkan kecepatan: ")
-    jarak = input("Masukkan jarak")
+    kecepatan = input('Masukkan kecepatan: ')
+    jarak = input('Masukkan jarak')
 
     vel_msg.linear.x = 0
     vel_msg.linear.y = 0
@@ -215,23 +246,24 @@ def downward():
         while(jarak_sekarang < jarak):
             # publish kecepatan
             pub.publish(vel_msg)
-            # ambil waktu sekarang 
+            # ambil waktu sekarang
             t1 = rospy.Time.now().to_sec()
             # kalkulasi jarak
             jarak_sekarang = kecepatan*(t1-t0)
-        # kecepatan 0 
+        # kecepatan 0
         vel_msg.linear.x = 0
         # paksa henti
         pub.publish(vel_msg)
 
+
 def counterclock():
-    pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10 )
+    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
     rospy.init_node('counterclock', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+    rate = rospy.Rate(10)  # 10hz
     vel_msg = Twist()
     # parameter
-    kecepatan = input("Masukkan kecepatan: ")
-    jarak = input("Masukkan jarak")
+    kecepatan = input('Masukkan kecepatan: ')
+    jarak = input('Masukkan jarak')
 
     vel_msg.linear.x = 0
     vel_msg.linear.y = 0
@@ -249,23 +281,24 @@ def counterclock():
         while(jarak_sekarang < jarak):
             # publish kecepatan
             pub.publish(vel_msg)
-            # ambil waktu sekarang 
+            # ambil waktu sekarang
             t1 = rospy.Time.now().to_sec()
             # kalkulasi jarak
             jarak_sekarang = kecepatan*(t1-t0)
-        # kecepatan 0 
+        # kecepatan 0
         vel_msg.linear.x = 0
         # paksa henti
         pub.publish(vel_msg)
 
+
 def clockwise():
-    pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10 )
+    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
     rospy.init_node('clockwise', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+    rate = rospy.Rate(10)  # 10hz
     vel_msg = Twist()
     # parameter
-    kecepatan = input("Masukkan kecepatan: ")
-    jarak = input("Masukkan jarak")
+    kecepatan = input('Masukkan kecepatan: ')
+    jarak = input('Masukkan jarak')
 
     vel_msg.linear.x = 0
     vel_msg.linear.y = 0
@@ -283,23 +316,24 @@ def clockwise():
         while(jarak_sekarang < jarak):
             # publish kecepatan
             pub.publish(vel_msg)
-            # ambil waktu sekarang 
+            # ambil waktu sekarang
             t1 = rospy.Time.now().to_sec()
             # kalkulasi jarak
             jarak_sekarang = kecepatan*(t1-t0)
-        # kecepatan 0 
+        # kecepatan 0
         vel_msg.linear.x = 0
         # paksa henti
         pub.publish(vel_msg)
 
+
 def hover():
-    pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10 )
+    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
     rospy.init_node('clockwise', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+    rate = rospy.Rate(10)  # 10hz
     vel_msg = Twist()
     # parameter
-    kecepatan = input("Masukkan kecepatan: ")
-    jarak = input("Masukkan jarak")
+    kecepatan = input('Masukkan kecepatan: ')
+    jarak = input('Masukkan jarak')
 
     vel_msg.linear.x = 0
     vel_msg.linear.y = 0
@@ -317,14 +351,15 @@ def hover():
         while(jarak_sekarang < jarak):
             # publish kecepatan
             pub.publish(vel_msg)
-            # ambil waktu sekarang 
+            # ambil waktu sekarang
             t1 = rospy.Time.now().to_sec()
             # kalkulasi jarak
             jarak_sekarang = kecepatan*(t1-t0)
-        # kecepatan 0 
+        # kecepatan 0
         vel_msg.linear.x = 0
         # paksa henti
         pub.publish(vel_msg)
+
 
 if __name__ == '__main__':
     try:
