2de053515b9ab8a5df38f7e6e67cd9df8e28e522
yanottamao <oktatama4@gmail.com>
2020-11-15 04:33:23 +0700

    Merubah pemanggilan slam sebagai fungsi terpisah

--- ardimaging_foredit.py
+++ ardimaging_foredit.py
@@ -10,5 +10,19 @@ os.system(
 # from subprocess import call
 # call(['ls', '-l'])
 
-call(['rosrun', 'lsd_slam_core', 'live_slam',
-      'image:/ardrone/front/image_raw', '_calib:/file'])
+
+def slam():
+    global slam_run
+    # testing
+    # slam_run = 'Jalankan slam'
+    # print(slam_run)
+    slam_run = call(['rosrun', 'lsd_slam_core', 'live_slam',
+                     'image:/ardrone/front/image_raw', '_calib:/file'])
+
+
+def main():
+    slam()
+
+
+if __name__ == "__main__":
+    main()
