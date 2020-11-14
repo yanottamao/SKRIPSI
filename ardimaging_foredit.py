afc9203b07e241a9ff052e215ed577407b4de724
yanottamao <oktatama4@gmail.com>
2020-11-15 04:19:19 +0700

    Tambah alternatif pemanggilan lsd slam di terminal

--- ardimaging_foredit.py
+++ ardimaging_foredit.py
@@ -1,5 +1,14 @@
 # actually not recommended
 # need to fix formatting
 import os
+from subprocess import call
 os.system(
     "rosrun lsd_slam_core live_slam image:/ardrone/front/image_raw _calib:/file")
+
+# another way to call command to terminal
+# reference https://stackoverflow.com/questions/3730964/python-script-execute-commands-in-terminal
+# from subprocess import call
+# call(['ls', '-l'])
+
+call(['rosrun', 'lsd_slam_core', 'live_slam',
+      'image:/ardrone/front/image_raw', '_calib:/file'])
