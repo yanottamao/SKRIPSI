43fa06554f654bda4ee7f544b32ea80b5a0887b9
yanottamao <oktatama4@gmail.com>
2020-12-02 06:16:30 +0700

    Untuk editing

---
+++ mainloop_edit_streamlined.py
@@ -0,0 +1,241 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+from std_msgs.msg import Empty
+from geometry_msgs.msg import Twist
+
+
+def mode_bergerak(perintah_bergerak, kecepatan):
+    while perintah_bergerak != 'q':
+        global lx
+        global ly
+        global lz
+        global az
+
+        print('\nMenu pergerakan')
+        print('q. Keluar')
+        print('u. Diam di udara')
+        print('i. Maju')
+        print('k. Mundur')
+        print('j. Ke kiri')
+        print('l. Ke kanan')
+        print('y. Naik')
+        print('t. Turun')
+        print('p. Searah jarum jam')
+        print('o. Berlawanan jarum jam')
+        perintah_bergerak = input('Masukkan perintah: ')
+        print('')
+        print('Perintah: ' + perintah_bergerak)
+        if perintah_bergerak in ['q', 'u', 'i', 'k', 'j', 'l', 'y', 't', 'p', 'o']:
+            # drone diam di udara hover
+            if perintah_bergerak == 'u':
+                print('Drone diam\n')
+                lx = 0
+                ly = 0
+                lz = 0
+                az = 0
+                return lx, ly, lz, az
+
+            # drone maju
+            elif perintah_bergerak == 'i':
+                print('Drone maju\n')
+                lx = abs(kecepatan)
+                ly = 0
+                lz = 0
+                az = 0
+                return lx, ly, lz, az
+
+            # drone mundur
+            elif perintah_bergerak == 'k':
+                print('Drone mundur\n')
+                lx = -abs(kecepatan)
+                ly = 0
+                lz = 0
+                az = 0
+                return lx, ly, lz, az
+
+            # drone ke kiri
+            elif perintah_bergerak == 'j':
+                print('Drone ke kiri\n')
+                lx = 0
+                ly = abs(kecepatan)
+                lz = 0
+                az = 0
+                return lx, ly, lz, az
+
+            # drone ke kanan
+            elif perintah_bergerak == 'l':
+                print('Drone ke kanan\n')
+                lx = 0
+                ly = -abs(kecepatan)
+                lz = 0
+                az = 0
+                return lx, ly, lz, az
+
+            # drone naik
+            elif perintah_bergerak == 'y':
+                print('Drone naik\n')
+                lx = 0
+                ly = 0
+                lz = abs(kecepatan)
+                az = 0
+                return lx, ly, lz, az
+
+            # drone turun
+            elif perintah_bergerak == 't':
+                print('Drone turun\n')
+                lx = 0
+                ly = 0
+                lz = -abs(kecepatan)
+                az = 0
+                return lx, ly, lz, az
+
+            # drone berputar searah jarum jam
+            elif perintah_bergerak == 'p':
+                print('Drone searah jarum jam\n')
+                lx = 0
+                ly = 0
+                lz = 0
+                az = -abs(kecepatan)
+                return lx, ly, lz, az
+
+            # drone berputar berlawanan jarum jam
+            elif perintah_bergerak == 'o':
+                print('Drone berlawanan jarum jam\n')
+                lx = 0
+                ly = 0
+                lz = 0
+                az = abs(kecepatan)
+                return lx, ly, lz, az
+
+            # kembali ke menu sebelumnya
+            elif perintah_bergerak == 'q':
+                print('Keluar dari menu pergerakan')
+                lx = 0
+                ly = 0
+                lz = 0
+                az = 0
+                return lx, ly, lz, az
+                break
+        else:
+            print('Masukkan sesuai perintah!')
+            lx = 0
+            ly = 0
+            lz = 0
+            az = 0
+            return lx, ly, lz, az
+
+
+def standar(perintah_standar):
+    while perintah_standar != 'q':
+        global mode
+        print('\nMenu Awal')
+        print('q. Keluar')
+        print('f. Takeoff')
+        print('g. Landing')
+        print('h. Mode bergerak')
+        perintah_standar = input('Masukkan perintah: ')
+        print('')
+        print('Perintah: ' + perintah_standar)
+        if perintah_standar in ['q', 'f', 'g', 'h']:
+
+            # menuju ke menu pergerakan
+            if perintah_standar == 'h':
+                print('Menuju menu pergerakan')
+                mode_bergerak(perintah_standar)
+                mode = '/cmd_vel'
+                mode_bergerak(perintah_bergerak, kecepatan)
+                arah_bergerak(mode, kecepatan, lx, ly, lz, az)
+
+            # drone takeoff
+            elif perintah_standar == 'f':
+                print('Drone takeoff\n')
+                mode = 'ardrone/takeoff'
+                state_terbang(mode)
+
+            # drone landing
+            elif perintah_standar == 'g':
+                print('Drone landing\n')
+                mode = 'ardrone/land'
+                state_terbang(mode)
+
+            # kembali ke menu sebelumnya
+            elif perintah_standar == 'q':
+                print('Keluar dari menu awal')
+                break
+        else:
+            print('Masukkan sesuai perintah!')
+
+# fungsi untuk inisialisasi kecepatan default
+
+
+def kecepatan_default():
+    global kecepatan
+    kecepatan = float(input('Masukkan kecepatan antara 0.1 - 1: '))
+    return kecepatan
+
+
+def state_terbang(mode):
+    # not sure what queue_size do
+    pub = rospy.Publisher(mode, Empty, queue_size=10)
+    rospy.init_node('terbang', anonymous=True)    # should be just once
+    rate = rospy.Rate(10)   # should be frequency of transfer rate at 10 hz ?
+    while not rospy.is_shutdown():
+        pub.publish(Empty())
+        rate.sleep()
+
+
+def arah_bergerak(mode, kecepatan, lx, ly, lz, az):
+    pub = rospy.Publisher(mode, Twist, queue_size=10)
+    # rospy.init_node('bergerak', anonymous = True) # should be not necessary
+    rate = rospy.Rate(10)
+    global vel_msg = Twist()
+
+    vel_msg.linear.x = lx
+    vel_msg.linear.y = ly
+    vel_msg.linear.z = lz
+    vel_msg.angular.x = 0
+    vel_msg.angular.y = 0
+    vel_msg.angular.z = az
+
+    while not rospy.is_shutdown():
+        pub.publish(vel_msg)
+        rate.sleep()    # not sure
+
+
+def main():
+    '''
+    # print('Kendali Ar.Drone dengan Python')
+    # modeterbang() ->  standar(perintah_standar)
+    print('Kendali Ar.Drone dengan Python')
+    modeterbang()
+    '''
+    while perintah_main != 'q':
+        print('\nMenu utama')
+        print('q. Keluar')
+        print('1. Mulai')
+        perintah_main = input('Masukkan perintah: ')
+        print('')
+        print('Perintah: ' + perintah_main)
+        if perintah_main in ['1', 'q']:
+            if perintah_main == '1':
+                print('Menuju menu awal')
+                standar(perintah_standar)
+            elif perintah_main == 'q':
+                print('Keluar dari program')
+                break
+        else:
+            print('Masukkan sesuai perintah!')
+
+
+if __name__ == '__main__':
+    try:
+        global perintah_main
+        global perintah_bergerak
+        global perintah_standar
+        perintah_main = ''
+        perintah_bergerak = ''
+        perintah_standar = ''
+        main(perintah_main)
+    except rospy.ROSInterruptException:
+        pass
